9825d2edb8790527cac16ff1f2b91e7f8a3dd111
Fukuyori-Kanta <b1701685@planet.kanazawa-it.ac.jp>
2021-06-11 18:48:34 +0900

    create_tmb_img#21

--- resources/app/python/file_io.py
+++ resources/app/python/file_io.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import shutil
 
 def getEncode(filepath):
     encs = "iso-2022-jp euc-jp sjis utf-8-sig".split()
@@ -9,7 +11,6 @@ def getEncode(filepath):
             except UnicodeDecodeError:
                 continue
         return enc
-
 
 def read_csv(file_path, needs_skip_header=False):
     """
@@ -43,7 +44,40 @@ def read_csv(file_path, needs_skip_header=False):
         l.append(row)
     
     return l
+
+def read_favo(file_path, needs_skip_header=False):
+    """
+    CSVファイルを読み込んで、その結果を返す関数
+
+    Parameters
+    ----------
+    file_path : str
+        読み込むCSVファイルのパス
+
+    needs_skip_header : bool, default False
+        ヘッダーを読み飛ばすかどうか
+        
+    Returns
+    -------
+    l : list
+        読み込んだ結果を返すリスト
+    """
+    # 文字コードを取得
+    enc = getEncode(file_path)
+
+    csvfile = open(file_path, 'r', encoding=enc)
+    reader = csv.reader(csvfile, delimiter=',')
+
+    # ヘッダーを読み飛ばしたい時
+    if needs_skip_header:
+        header = next(reader)  
+
+    l = []
+    for row in reader:
+        l.append(row)
     
+    return l
+
 def write_csv(data, dest_path):
     """
     データを受け取り、CSVに書き出す関数
@@ -64,3 +98,40 @@ def write_csv(data, dest_path):
 
         else:   # 1次元配列の場合
             writer.writerow(data)
+
+def write_ranking_data(data, dest_path, i):
+    """
+    ランキングのデータを受け取り、CSVに書き出す関数
+
+    Parameters
+    ----------
+    data : list
+        出力するデータ
+        
+    dest_path : str
+        保存先フォルダのパス
+    """
+    if i == 0:
+        mode = 'w'
+    else:
+        mode = 'a'
+
+    with open(dest_path, mode, newline="", encoding='utf-8-sig') as f:
+        writer = csv.writer(f)
+
+        writer.writerows(data)
+
+def dest_folder_create(dest_path):
+    """
+    保存先フォルダを作成する関数
+
+    Parameters
+    ----------
+    dest_path : str
+        保存先フォルダのパス
+    """
+    # 保存先フォルダの作成
+    # 既に存在する場合は削除
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)    # フォルダ削除
+    os.mkdir(dest_path)     # 保存先フォルダの作成
---
+++ resources/app/python/scene2tmb.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+import shutil
+from logging import getLogger, StreamHandler, DEBUG
+import csv
+
+from file_io import dest_folder_create
+
+def save_thumbnail_img(input_video_path, result_thumbnail_path, scene):
+    """
+    動画を読み込み、フレームデータと動画情報を抽出する関数
+
+    Parameters
+    ----------
+    input_video_path : str
+        動画の入力パス   
+
+    Returns
+    -------
+    frames : numpy.ndarray
+        フレームデータ（動画の全画像データ）
+    
+    video_info : list 
+        動画データ [fps, width, height]
+    """
+    # --------------------------------------------------
+    # 動画の読み込み
+    # --------------------------------------------------
+    input_video_path = input_video_path + '\\' + scene   # 入力動画パス
+    cap = cv2.VideoCapture(input_video_path)
+
+    # ビデオキャプチャーが開けていない場合、例外を返す
+    if cap.isOpened() is False:
+        raise ValueError('読み込みエラー : 動画ID ' + input_video_path + 'が上手く読み取れません。')
+    
+    fps = cap.get(cv2.CAP_PROP_FPS)                 # FPS
+    width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)       # 幅
+    height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)     # 高さ
+    frame_count = cap.get(cv2.CAP_PROP_FRAME_COUNT) # 総フレーム数
+    sec = frame_count / fps                         # 秒数
+    
+    # --------------------------------------------------
+    # フレーム毎の画像情報をリストに格納
+    # --------------------------------------------------
+    n_frames = int(frame_count) # 総フレーム数 
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        # 最後まで取得出来なかった場合、そこまでを総フレームとして更新
+        if not ret:
+            #n_frames = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+            break
+
+        frames.append(frame)
+        if len(frames) == n_frames:
+            break
+
+    if cap.isOpened():
+        cap.release()   
+
+    # サムネ画像の保存    
+    index = scene.split('scene')[1].split('.mp4')[0]
+    cv2.imwrite(result_thumbnail_path + '\\thumbnail' + index + '.jpg', frames[-1])
+
+    cv2.destroyAllWindows()
+
+
+base = os.path.dirname(os.path.abspath(__file__))   # スクリプト実行ディレクトリ
+scene_path = os.path.normpath(os.path.join(base, r'..\result\scene'))   # シーン動画の保存先
+thumbnail_path = os.path.normpath(os.path.join(base, r'..\result\thumbnail')) # サムネイルの保存先
+
+dest_folder_create(thumbnail_path)
+video_id_list = os.listdir(scene_path)  # 動画IDリスト
+
+for video_id in video_id_list:
+    input_video_path = os.path.normpath(os.path.join(scene_path, video_id)) # 入力動画パス
+    result_thumbnail_path = os.path.normpath(os.path.join(thumbnail_path, video_id))    # サムネ結果保存パス
+
+    # サムネ画像の保存フォルダを作成
+    dest_folder_create(result_thumbnail_path)
+
+    scene_list = os.listdir(input_video_path)   # シーンリスト
+
+    # サムネ画像を保存
+    for scene in scene_list:
+        save_thumbnail_img(input_video_path, result_thumbnail_path, scene)
